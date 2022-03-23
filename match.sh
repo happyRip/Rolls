16e16737520b2963858608654dd8e4690ea558f9
Aleksander Borowski <aborbzbk@gmail.com>
2022-03-23 23:05:27 +0100

    Remove old

--- match.sh
+++ match.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 sum=0
-while read line; do
-    matched=$(grep -c "$line" $1)
-    $(( sum += matched ))
-done < <(echo "$FILECONTENT")
+while read -r line; do
+    matched=$(grep -wc "$line" "$1")
+    (( sum += matched ))
+done
 
 echo $sum
