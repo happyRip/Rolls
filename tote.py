e4f3086734d49d1b91e89700bd2e0730f390578f
Aleksander Borowski <aborbzbk@gmail.com>
2022-03-25 18:16:04 +0100

    Move tote to python

---
+++ tote.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+
+import argparse
+from collections import defaultdict
+
+import pandas as pd
+
+
+class Board():
+    def __init__(self) -> None:
+        self._key_values = []
+        self._stat_map = defaultdict(lambda: 0)
+        self._match_map = defaultdict(lambda: 0)
+
+    def parse_key_file(self, path: str) -> None:
+        self._key_file = open(path, 'r')
+
+        for line in self._key_file.readlines():
+            for v in line.split():
+                self._key_values.append(int(v))
+
+        self._key_file.close()
+
+    def update_stat_map(self, values: list[int]) -> None:
+        for key in values:
+            if key in self._key_values:
+                self._stat_map[key] += 1
+
+    def update_match_map(self, values: list[int]) -> None:
+        matched = 0
+        for key in values:
+            if key in self._key_values:
+                matched += 1
+        self._match_map[matched] += 1
+
+    def get_key_values(self) -> list[int]:
+        return self._key_values
+
+    def get_stat_map(self) -> dict[int, int]:
+        return self._stat_map
+
+    def get_match_map(self) -> dict[int, int]:
+        return self._match_map
+
+
+class Stats():
+    def __init__(self, match_map: dict[int, int]) -> None:
+        self._match_data_frame = pd.DataFrame.from_dict(match_map, dtype=int)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('key_file', nargs=1, type=str)
+    parser.add_argument('-f', '--input-files', nargs='+')
+
+    args = vars(parser.parse_args())
+    print(args)
+
+    board = Board()
+
+    key_file = args['key_file'][0]
+    if key_file != None:
+        board.parse_key_file(key_file)
+
+        print('key values:')
+        __import__('pprint').pprint(board.get_key_values())
+
+    input_files = args['input_files']
+    for file in input_files:
+        f = open(file, 'r')
+        for line in f.readlines():
+            values = []
+            for v in line.split():
+                values.append(int(v))
+
+            board.update_stat_map(values)
+            board.update_match_map(values)
+
+    print('stat map:')
+    __import__('pprint').pprint(board.get_stat_map())
+
+    print('match map:')
+    __import__('pprint').pprint(board.get_match_map())
+
+
+if __name__ == "__main__":
+    main()
